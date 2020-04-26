09f13aa188a704056dca0ffef11215f4416d68a2
TVBrowntown <32625383+TVBrowntown@users.noreply.github.com>
2020-04-26 14:27:03 +0930

    Add files via upload

---
+++ WOTLKGraphicBoost1.lua
@@ -0,0 +1,40 @@
+-- following cmds will entered on world load
+SlashCmdList["CONSOLE"]("farclip 4000");
+SlashCmdList["CONSOLE"]("horizonFarclipScale 6");
+SlashCmdList["CONSOLE"]("skycloudlod 3");
+SlashCmdList["CONSOLE"]("waterDetail 3");
+SlashCmdList["CONSOLE"]("reflectionMode 3");
+SlashCmdList["CONSOLE"]("rippleDetail 2");
+SlashCmdList["CONSOLE"]("shadowmode 3");
+SlashCmdList["CONSOLE"]("shadowtexturesize 2048");
+SlashCmdList["CONSOLE"]("particleDensity 100");
+SlashCmdList["CONSOLE"]("environmentDetail 150");
+SlashCmdList["CONSOLE"]("groundEffectDensity 256");
+SlashCmdList["CONSOLE"]("groundEffectDist 140");
+SlashCmdList["CONSOLE"]("groundEffectFade 1277");
+SlashCmdList["CONSOLE"]("spellEffectLevel 200");
+SlashCmdList["CONSOLE"]("ffxGlow 1");
+SlashCmdList["CONSOLE"]("ffxspecial 1");
+SlashCmdList["CONSOLE"]("ffxnetherworld 1");
+SlashCmdList["CONSOLE"]("ffxDeath 1");
+SlashCmdList["CONSOLE"]("sunshafts 2");
+SlashCmdList["CONSOLE"]("textureFilteringMode 5");
+SlashCmdList["CONSOLE"]("componentTextureLevel 9");
+SlashCmdList["CONSOLE"]("terrainMipLevel 0");
+SlashCmdList["CONSOLE"]("ssao 2");
+SlashCmdList["CONSOLE"]("ssaoblur 2");
+SlashCmdList["CONSOLE"]("weatherDensity 3");
+SlashCmdList["CONSOLE"]("gxmultiesample 8");
+SlashCmdList["CONSOLE"]("violencelevel 5");
+SlashCmdList["CONSOLE"]("gxApi D3D9EX");
+SlashCmdList["CONSOLE"]("worldBaseMip 0");
+SlashCmdList["CONSOLE"]("showfootprints 1");
+SlashCmdList["CONSOLE"]("showfootprintparticles 1");
+SlashCmdList["CONSOLE"]("extShadowQuality 5");
+SlashCmdList["CONSOLE"]("CameraDistanceMaxFactor 1");
+SlashCmdList["CONSOLE"]("cameraDistanceMaxFactor 49");
+SlashCmdList["CONSOLE"]("characterambient");
+
+
+
+
